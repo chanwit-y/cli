edf1a8f23d2bc74e2e563ef9666db7859583b55c
cy <chanwit.yimneam@hotmail.com>
2025-08-07 16:15:54 +0700

    Enhance ThemeProvider component by adding ThemeContext for textField props, improving context integration and type definitions in the UI library.

--- packages/ui/src/libs/@types/index.ts
+++ packages/ui/src/libs/@types/index.ts
@@ -2,10 +2,15 @@ import type { ComponentPropsWithoutRef } from "react";
 import { TextField as RadixTextField } from "@radix-ui/themes";
 import { type ThemeProps } from "@radix-ui/themes";
 
+export type ThemeContextType = {
+  textField?: TextFieldProps
+}
+
 export interface ThemeProviderProps {
   children: React.ReactNode;
   theme?: ThemeProps;
   className?: string;
+  textField?: TextFieldProps;
 }
 
 export interface TextFieldProps
--- packages/ui/src/libs/context/ThemeProvider.tsx
+++ packages/ui/src/libs/context/ThemeProvider.tsx
@@ -1,22 +1,26 @@
 
-import type { ThemeProviderProps } from '../@types'
+import { createContext } from 'react'
+import type { ThemeContextType, ThemeProviderProps } from '../@types'
 import { Theme } from '@radix-ui/themes'
 
 
+const ThemeContext = createContext<ThemeContextType>({})
 
-export function ThemeProvider({ 
-  children, 
-  theme = { 
-    appearance: 'light', 
-    accentColor: 'blue', 
+export function ThemeProvider({
+  children,
+  theme = {
+    appearance: 'light',
+    accentColor: 'blue',
     radius: 'small',
-     
   },
-  className 
+  className,
+  textField
 }: ThemeProviderProps) {
   return (
-    <Theme panelBackground="translucent" {...theme} className={className}>
-      {children}
-    </Theme>
+    <ThemeContext value={{ textField }}>
+      <Theme panelBackground="translucent" {...theme} className={className}>
+        {children}
+      </Theme>
+    </ThemeContext>
   )
 }
